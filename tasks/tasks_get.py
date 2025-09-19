73ad235201a03b4395f27f7354107e9bf95e84c6
NishantGada <nishant.gada160@gmail.com>
2025-09-19 02:17:37 -0400

    refactor: fixed fetch tasks api error to actually fetch tasks using task_id

--- tasks/tasks_get.py
+++ tasks/tasks_get.py
@@ -12,8 +12,17 @@ def get_tasks_by_user_id():
     cursor = connection.cursor(dictionary=True)
     auth_user = request.user["id"]
 
-    cursor.execute("SELECT * FROM user_tasks WHERE user_id = %s", (auth_user,))
-    tasks = cursor.fetchall()
+    cursor.execute("SELECT task_id FROM user_tasks WHERE user_id = %s", (auth_user,))
+    task_ids = cursor.fetchall()
+    print("task_ids: ", task_ids)
+
+    tasks = []
+    
+    for task_id in task_ids:
+        cursor.execute("SELECT * FROM tasks WHERE task_id = %s", (task_id["task_id"],))
+        task = cursor.fetchone()
+        tasks.append(task)
+
 
     return jsonify({
         "success": True, 
