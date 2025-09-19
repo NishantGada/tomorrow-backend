c469059e6da6007ed91bd378344580e708f5b645
NishantGada <nishant.gada160@gmail.com>
2025-09-19 02:18:04 -0400

    setup: api to fetch tomorrow's date

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from datetime import datetime, timedelta
 
 from config.dbconfig import *
 from auth import auth_bp
@@ -13,9 +14,32 @@ app.register_blueprint(users_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(tasks_bp)
 
-@app.route('/')
+
+@app.route("/")
 def root():
     return jsonify({"message": "Welcome to Tomorrow!"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+
+@app.route("/tomorrow-date", methods=["GET"])
+def fetch_tomorrow_date():
+    tomorrow = datetime.now() + timedelta(days=1)
+
+    # extract day and month
+    day = tomorrow.day
+    month = tomorrow.month
+    month_name = tomorrow.strftime("%B")
+
+    return (
+        jsonify(
+            {
+                "success": True,
+                "message": "Tomorrow's date fetched successfully",
+                "data": {"day": day, "month": month, "month_name": month_name},
+            }
+        ),
+        200,
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
